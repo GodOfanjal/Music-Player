13e598b2116d55429b7f056fd9ed009fb8e78c5e
Barath <95070448+GodOfanjal@users.noreply.github.com>
2022-04-01 19:37:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/godofanjal/Music-Player.git /Music-Player
-cd /Music-Player
+git clone https://github.com/godofanjal/Music-Player.git main.py
+cd main.py
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
